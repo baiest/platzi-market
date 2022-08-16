db764426ff81f47ad65d5ac16ebcc5ddbaccc620
Baiest <juanballesteros2001@gmail.com>
2022-08-16 12:07:20 -0500

    Add: ProductoRepository

---
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -0,0 +1,35 @@
+package com.platzi.market.persistence;
+
+import com.platzi.market.persistence.crud.ProductoCrudRepository;
+import com.platzi.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProductoRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll(){ return (List<Producto>) productoCrudRepository.findAll();}
+
+    public List<Producto> getByCategoria(Long idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(Long idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Long idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+}
